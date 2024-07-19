1e036958c6471fa23f14c678e0144bf241ec9957
David <davidbrundukov228@gmail.com>
2024-07-19 19:54:35 +0300

    commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from settings import Settings
 from glob import glob
 
+import plan_16_07_24
+
 
 def get_bird_species():
     main_path = Settings.main_path
@@ -19,3 +21,4 @@ def get_bird_species():
 if len(get_bird_species()) == 0:
     print('Не тот компьютер')
     input('>>><<<')
+# plan_16_07_24.learn_model_2()
